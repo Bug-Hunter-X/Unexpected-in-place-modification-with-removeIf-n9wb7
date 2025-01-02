680ae563f25a7520a603f77a68cae14057db7ce5
BugHunter-X <askzenai@gmail.com>
2025-01-03 00:48:05 +0100

    Add removeIfBug.kt file

---
+++ removeIfBug.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val list = mutableListOf(1, 2, 3, 4, 5)
+    list.removeIf { it % 2 == 0 }
+    println(list) // Output: [1, 3, 5]
+
+    val map = mutableMapOf("a" to 1, "b" to 2, "c" to 3)
+    map.removeIf { it.value % 2 == 0 }
+    println(map) // Output: {a=1, c=3}
+}
